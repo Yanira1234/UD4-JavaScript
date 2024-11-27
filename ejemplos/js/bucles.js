b0089c3ed54838623c7eb83b0533b37a3fa00a57
Yanira1234 <yanira.procas.07@gmail.com>
2024-11-27 10:45:45 +0100

    nueva teoria

--- ejemplos/js/bucles.js
+++ ejemplos/js/bucles.js
@@ -7,16 +7,17 @@ while (contador < 10){
     contador++;
 }
 
-//Quiero que el usuario meta un numero y que si no esta entre 0 y 10 le vuelva a pedir el numero.
+
+//Quiero que el usuario meta un numero y que si no esta entre 0 y 10 le vuelva a pedir el numero. ------------------------
 var numero = prompt("Escribe un numero entre 0 y 10");
 
 while(numero < 0 || numero > 10){
     numero = prompt("Escribe un numero entre 0 y 10");
 }
 document.writeln("Su numero es " + numero);
-*/
 
-//Suma los numeros del 1 al 5 (1+2+3+4+5)
+
+//Suma los numeros del 1 al 5 (1+2+3+4+5) --------------------------------------------------------------------------------
 var suma = 0
 var i = 1
 
@@ -25,6 +26,13 @@ while(i <= 5){
     i++;
 }
 document.write(suma);
-
 //Sin terminar
 
+*/
+
+//USO DEL "for" --------------------------------------------------------------------------------------------------------------------------------------------------------
+//Bucle for para sacar por pantalla los numeros del 10 al 0
+var numero;
+for(numero=10; numero>=0; numero--) {
+    document.write(numero + "<br>");
+}
