f4606ef546c289df2810bf53d9d4e029620a17d4
Yanira1234 <yanira.procas.07@gmail.com>
2024-11-27 10:53:25 +0100

    hago el ej4 con for y lo corrijo

--- ejercicios/practica3/ejercicio4/script.js
+++ ejercicios/practica3/ejercicio4/script.js
@@ -1,7 +1,19 @@
+
+/*
 var numero = prompt("Escribe un numero");
 numero = parseInt(numero);
 
 while(numero < 100){
     document.write(numero + "<br>");
     numero++;
+}
+*/
+
+//Con for: -------------------------------------------------
+var numero = prompt("Escribe un numero");
+numero = parseInt(numero);
+var i;
+
+for(i = numero; i < 100; i++) {
+    document.write(i + "<br>");
 }
