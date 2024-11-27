2fbbcdc816dd2e4c4c563402d1803f602a81666b
Yanira1234 <yanira.procas.07@gmail.com>
2024-11-27 12:00:22 +0100

    correccion ej12

--- ejercicios/practica3/ejercicio12/script.js
+++ ejercicios/practica3/ejercicio12/script.js
@@ -1,8 +1,12 @@
 var numero = prompt("Escribe un numero");
 numero = parseInt(numero);
 var i = 0
-
+/*
 while(i <= 10){
     document.write(numero + " x " + i + " = " + (numero * i) + "<br>");
     i++;
+}*/
+
+for(; i <= 10; i++){
+    document.write(numero + " x " + i + " = " + (numero * i) + "<br>");
 }
