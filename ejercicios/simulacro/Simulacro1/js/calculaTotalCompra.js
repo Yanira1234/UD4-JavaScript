c11735dba7db30b80c625098e00b597a28b21ae9
Yanira1234 <yanira.procas.07@gmail.com>
2024-11-28 13:51:34 +0100

    correccion del simulacro

---
+++ ejercicios/simulacro/Simulacro1/js/calculaTotalCompra.js
@@ -0,0 +1,35 @@
+var total = 0
+var precio = prompt("Introduzca el precio del artículo");
+precio = parseInt(precio);
+
+while(precio != 0){
+    if(isNaN(precio)){
+        alert("Lo siento solo se admiten numeros enteros");
+    }else if(precio < 0){
+        alert("Lo siento no se permiten números negativos");
+    }else{
+        total = total + precio;
+    }
+    precio = prompt("Introduzca el precio del artículo");
+    precio = parseInt(precio);
+}
+document.write("El total de su compra ha sido: " + total);
+
+//Alternativa:
+/*
+var total = 0
+var precio = 1
+
+while(precio != 0){
+    precio = prompt("Introduzca el precio del artículo");
+    precio = parseInt(precio);
+    if(isNaN(precio)){
+        alert("Lo siento solo se admiten numeros enteros");
+    }else if(precio < 0){
+        alert("Lo siento no se permiten números negativos");
+    }else{
+        total = total + precio;
+    }
+}
+document.write("El total de su compra ha sido: " + total);
+*/
