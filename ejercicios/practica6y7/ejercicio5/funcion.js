c50e1f8d43aae056b7de74467f49fd8424c8e2ed
Yanira1234 <yanira.procas.07@gmail.com>
2024-12-03 10:16:05 +0100

    ej.5 en proceso

---
+++ ejercicios/practica6y7/ejercicio5/funcion.js
@@ -0,0 +1,7 @@
+function funcion() {
+    var numero1 = parseInt(document.getElementById("numero1").value);
+    var numero2 = parseInt(document.getElementById("numero1").value);
+    
+
+    var suma = document.getElementById("suma").value = (numero1 + numero2);
+}
