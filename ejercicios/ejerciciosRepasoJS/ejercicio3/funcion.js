818dec846414782c645593bfc02fb71ddb0f9262
Yanira1234 <yanira.procas.07@gmail.com>
2024-12-04 12:30:15 +0100

    ej4 en proceso

---
+++ ejercicios/ejerciciosRepasoJS/ejercicio3/funcion.js
@@ -0,0 +1,6 @@
+function escribeNombre(){
+    var nombre = document.getElementById("nombre").value;
+    var ciclo = document.getElementById("ciclo").value;
+
+    alert("Su nombre es " + nombre + " y usted estudia " + ciclo);
+}
---
+++ ejercicios/ejerciciosRepasoJS/ejercicio4/funcion.js
@@ -0,0 +1,7 @@
+function funcion(){
+    var precio = document.getElementById("precio").value;
+    var iva = document.getElementById("iva").value;
+    precio = parseFloat(precio);
+
+    document.getElementById("p").innerHTML = ("El precio total con IVA es: " + (precio + (precio * iva)));
+}
