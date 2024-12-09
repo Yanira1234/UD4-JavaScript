e71607bbb414e04228bebd364a1415616e9a9c7d
Yanira1234 <yanira.procas.07@gmail.com>
2024-12-09 19:39:52 +0100

    Preparacion para el examen

--- ejercicios/ejerciciosRepasoJS/ejercicio7/funcion.js
+++ ejercicios/ejerciciosRepasoJS/ejercicio7/funcion.js
@@ -1,40 +1,31 @@
+
 function funcion(){
-
-    //NO FUNCIONA NADA
-
     var peso = document.getElementById("peso").value;
     var altura = document.getElementById("altura").value;
-   /* peso = parseFloat(peso);
-    altura = parseFloat(altura);*/
+    peso = parseFloat(peso);
+    altura = parseFloat(altura);
 
-    peso = parseInt(peso);
-    altura = parseInt(altura);
+    var imc = (peso / (altura * altura));
+    alert(imc);
 
-    var imc = peso;
-    document.getElementById(h3).innerHTML = "Algo es " + imc;
-/*
+
     if(imc < 18.5) {
-        document.getElementById(h3).innerHTML = "Bajo peso";
-        document.getElementById(h3).className = "azul";
+        document.getElementById("h3").innerHTML = "Bajo peso";
+        document.getElementById("h3").className = "azul";
     }else if(imc < 25) {
-        document.getElementById(h3).innerHTML = "Peso normal";
-        document.getElementById(h3).className = "verde";
+        document.getElementById("h3").innerHTML = "Peso normal";
+        document.getElementById("h3").className = "verde";
     }else if(imc < 30) {
-        document.getElementById(h3).innerHTML = "Sobrepeso";
-        document.getElementById(h3).className = "rojo";
+        document.getElementById("h3").innerHTML = "Sobrepeso";
+        document.getElementById("h3").className = "rojo";
     }else {
-        document.getElementById(h3).innerHTML = "Obesidad";
-        document.getElementById(h3).className = "rojo";
+        document.getElementById("h3").innerHTML = "Obesidad";
+        document.getElementById("h3").className = "rojo";
     }
 
 
 
-
-
-
-
-
-    /*
+/*NO FUNCIONA
     switch(imc) {
         case imc < 18,5:
             document.getElementById(h3).innerHTML = "Bajo peso";
@@ -55,5 +46,6 @@ function funcion(){
             document.getElementById(h3).innerHTML = "Obesidad";
             document.getElementById(h3).className = "rojo";
             break;
-    }*/
+    }
+*/
 }
